f571ce711c699b0e591f1224b1f21cb90e9897e6
Daeyoung Kim <daykim22@gmail.com>
2022-11-24 16:16:08 +0900

    Test template 추가
    Signed-off-by: Daeyoung Kim <daykim22@gmail.com>

--- starter/GitHubSearchTests/GitHubSearchTests.swift
+++ starter/GitHubSearchTests/GitHubSearchTests.swift
@@ -1,4 +1,11 @@
 import XCTest
 
+import ComposableArchitecture
+
+@testable import GitHubSearch_starter
+
+@MainActor
 final class GitHubSearchTests: XCTestCase {
+  func test_repoSearchResults_whenSearchButtonTapped() async {
+  }
 }
